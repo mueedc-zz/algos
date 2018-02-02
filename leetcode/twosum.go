7d9c3b752333025f241e77bd96d18afdf64717de
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2018-02-02 11:52:08 -0500

    solution two sum in Go

---
+++ leetcode/twosum.go
@@ -0,0 +1,28 @@
+package program
+
+import "sort"
+
+func TwoSum(nums []int, target int) []int {
+	indexs := make([]int, 2)
+	hash := map[int]int{}
+
+	for i := range nums {
+		hash[target-nums[i]] = i
+	}
+
+	for i := range nums {
+		index, ok := hash[nums[i]]
+		if ok {
+			if i == index {
+				continue
+			}
+			indexs[0] = index
+			indexs[1] = i
+			sort.Ints(indexs)
+			break
+		}
+		continue
+	}
+
+	return indexs
+}
