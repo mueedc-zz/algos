64b2f571af0973e1afff529d68a78211db0edeee
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2018-02-22 12:29:59 -0500

    find first unique character using hashtable

---
+++ leetcode/firstUniqueChar.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const firstUniqChar = function(s) {
+    const dict = {}
+    // create hashtable with number of times character appears in string
+    for (let i = 0; i < s.length; i++) {
+        if (!dict.hasOwnProperty(s[i])) dict[s[i]] = 1
+        else dict[s[i]]++
+    }
+    
+    //find first instance of unique character and return index
+    for (let key in dict) {
+        if (dict[key] === 1) return s.indexOf(key)
+    }
+    
+    // if no unique character
+    return -1
+}
