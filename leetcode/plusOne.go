b4b8cbcdac83354ce22e80839f77e0bf1af2aa33
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2018-02-24 11:54:15 -0500

    solution

---
+++ leetcode/plusOne.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		} else {
+			digits[i] = 0
+		}
+	}
+	arr := make([]int, len(digits)+1)
+	arr[0] = 1
+	return arr
+}
