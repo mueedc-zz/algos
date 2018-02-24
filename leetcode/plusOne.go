1c0bf707985690e38f0b65696949fecbcee80777
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2018-02-24 12:00:06 -0500

    remove unnecessary lines

--- leetcode/plusOne.go
+++ leetcode/plusOne.go
@@ -5,9 +5,9 @@ func plusOne(digits []int) []int {
 		if digits[i] < 9 {
 			digits[i]++
 			return digits
-		} else {
-			digits[i] = 0
 		}
+		digits[i] = 0
+
 	}
 	arr := make([]int, len(digits)+1)
 	arr[0] = 1
