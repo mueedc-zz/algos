5c2bd2e1e9bae758e3b01f6c3ce70b5fca4b190e
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2018-03-12 16:32:07 -0400

    insertion sort algorithm

---
+++ algoexpert/easy/insertionSort.go
@@ -0,0 +1,10 @@
+package algoexpert
+
+func InsertionSort(array []int) []int {
+	for i := range array {
+		for j := i; j > 0 && array[j] < array[j-1]; j-- {
+			array[j], array[j-1] = array[j-1], array[j]
+		}
+	}
+	return array
+}
